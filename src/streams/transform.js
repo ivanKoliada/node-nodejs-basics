685ed1430c97c8eaa0d608666a449d98ab3f214b
justgrace <ivan.koliada69@gmail.com>
2022-05-30 20:46:48 +0300

    feat: complete streams task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,27 @@
+import { Transform, pipeline } from 'stream';
+import { stdin, stdout } from 'process';
+
+const reverseStream = new Transform({
+  transform (chunk, _, callback) {
+    const reversedData = chunk.toString()
+      .trim()
+      .split('')
+      .reverse()
+      .join('');
+    this.push(`${reversedData}\n`);
+    callback();
+  }
+});
+
 export const transform = async () => {
-    // Write your code here 
+  pipeline(stdin, reverseStream, stdout, (err) => {
+    if (err) {
+      console.error('Pipeline failed.', err);
+    } else {
+      console.log('Pipeline succeeded.');
+    }
+  });
+  // stdin.pipe(reverseStream).pipe(stdout);
 };
+
+transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,12 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { stdin } from 'process';
+
 export const write = async () => {
-    // Write your code here 
+  const pathToFile = path.join('files', 'fileToWrite.txt');
+  const writableStream = fs.createWriteStream(pathToFile, 'utf8');
+
+  stdin.pipe(writableStream);
 };
+
+write();
