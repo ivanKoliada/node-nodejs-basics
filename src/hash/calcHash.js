d85cf11a1bcbb0b6d94b5600ade5ab1f00fc513a
justgrace <ivan.koliada69@gmail.com>
2022-05-30 21:10:50 +0300

    feat: complete hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,20 @@
+import * as crypto from 'crypto';
+import * as fs from 'fs';
+import { join } from 'path';
+
+const pathToFile = join('files', 'fileToCalculateHashFor.txt');
+
 export const calculateHash = async () => {
-    // Write your code here 
+  fs.readFile(pathToFile, 'utf8', (_, data) => {
+    const hash = crypto.createHash('sha256')
+      .update(data)
+      .digest('hex');
+
+    console.log(hash); 
+  });
 };
+
+calculateHash();
+
+
+
