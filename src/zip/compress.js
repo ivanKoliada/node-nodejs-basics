4994965cc29a22e9a6a06c73c8769830cec8211b
justgrace <ivan.koliada69@gmail.com>
2022-05-30 20:58:03 +0300

    feat: complete zip task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,23 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { join } from 'path';
+import { pipeline } from 'stream';
+import { createGzip } from 'zlib';
+
+const gzip = createGzip();
+const pathToFile = join('files', 'fileToCompress.txt');
+const pathToZipFile = join('files', 'archive.gz');
+
+const readableStream = createReadStream(pathToFile);
+const writableStream = createWriteStream(pathToZipFile);
+
 export const compress = async () => {
-    // Write your code here 
+  pipeline(readableStream, gzip, writableStream, (err) => {
+    if (err) {
+      console.error('Pipeline failed.', err);
+    } else {
+      console.log('Pipeline succeeded.');
+    }
+  }) 
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,25 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { join } from 'path';
+import { pipeline } from 'stream';
+import { createUnzip } from 'zlib';
+
+const gUnzip = createUnzip();
+const pathToZipFile = join('files', 'archive.gz');
+const pathToFile = join('files', 'fileToCompress.txt');
+
+const readableStream = createReadStream(pathToZipFile);
+const writableStream = createWriteStream(pathToFile);
+
 export const decompress = async () => {
-    // Write your code here 
+  pipeline(readableStream, gUnzip, writableStream, (err) => {
+    if (err) {
+      console.error('Pipeline failed.', err);
+    } else {
+      console.log('Pipeline succeeded.');
+    }
+  })
 };
+
+decompress();
+
+
