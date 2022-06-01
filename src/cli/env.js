43fbdf3b717bd0fae413c7ebf9a097a896b01c71
justgrace <ivan.koliada69@gmail.com>
2022-06-01 14:25:24 +0300

    feat: add environment variables

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,7 @@
+process.env.RSS_VAR1 = 'value1';
+process.env.RSS_VAR2 = 'value2';
+process.env.RSS_VAR3 = 'value3';
+
 export const parseEnv = () => {
   const objEnv = process.env;
   const arrEnvVar = Object.keys(objEnv)
