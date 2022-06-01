8e21677d8963bdfbe42aae432c353830d8744b14
justgrace <ivan.koliada69@gmail.com>
2022-06-01 13:38:10 +0300

    feat: complete cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,15 @@
+import * as url from 'url';
+import { fork } from 'child_process';
+import { join } from 'path';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const pathToFile = join(__dirname, 'files', 'script.js');
+
+const args = process.argv.slice(2);
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = fork(pathToFile, [...args]);
+  child.on('message', ()=> {})
 };
+
+spawnChildProcess(args);
