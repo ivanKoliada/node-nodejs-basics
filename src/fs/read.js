f0e7b0f03b00767efe8e4fb1f304c7c2260e4e4a
justgrace <ivan.koliada69@gmail.com>
2022-06-03 00:00:35 +0300

    refactor: move log to then

--- src/fs/read.js
+++ src/fs/read.js
@@ -6,11 +6,11 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const pathToFile = join(__dirname, 'files', 'fileToRead.txt');
 
 export const read = async () => {
-  const data = await readFile(pathToFile, 'utf8')
+  await readFile(pathToFile, 'utf8')
+    .then((data) => console.log('\x1b[34m', data, '\x1b[0m'))
     .catch(() => {
     throw new Error('FS operation failed')
   });
-  console.log('\x1b[34m', data, '\x1b[0m');
 };
 
 read();
