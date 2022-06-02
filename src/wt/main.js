2042f51451f0fce0ad51dca85f8da9c4f50e05c1
justgrace <ivan.koliada69@gmail.com>
2022-06-02 20:15:23 +0300

    feat: complete wt task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,28 @@
-export const performCalculations = async () => {
-    // Write your code here
-};
+import * as url from 'url'; 
+import { join } from 'path';
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
+const cores = cpus().length;
+const num = 10;
+const result = [];
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const pathToFile = join(__dirname, 'worker.js');
+
+export const performCalculations = async () => { 
+  for (let i = 0; i < cores; i++) {
+    const value = new Promise(resolve => {
+      const worker = new Worker(pathToFile, { workerData: num + i });
+      worker.on('message', msg => resolve({status: 'resolved', data: msg}));
+      worker.on('error', msg => resolve({status: 'error', data: null}));
+    });
+
+    result.push(await value)
+  }
+
+  console.log(result);
+ 
+}
+
+performCalculations();
