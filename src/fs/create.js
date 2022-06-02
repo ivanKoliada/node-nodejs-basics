e0d8be9aeab36fe7425a58aef16edfae46ca870a
justgrace <ivan.koliada69@gmail.com>
2022-06-02 20:48:34 +0300

    fix: add file content

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,13 +1,14 @@
 import * as url from 'url'; 
-import { open } from 'fs/promises';
+import { writeFile } from 'fs/promises';
 import { join } from 'path';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const pathToFile = join(__dirname, 'files', 'fresh.txt');
+const content = 'I am fresh and young';
 
 
 export const create = async () => {
-  open(pathToFile, 'wx').catch(() => {
+  writeFile(pathToFile, content, { flag: 'wx'}).catch(() => {
     throw new Error('FS operation failed')
   });  
 };
