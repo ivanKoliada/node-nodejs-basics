7b66cb3082cf0802948771d0e337304174acbe10
justgrace <ivan.koliada69@gmail.com>
2022-06-02 21:19:57 +0300

    fix: throw error

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,6 @@
 import * as url from 'url'; 
 import { rename as renameFile } from 'fs/promises';
+import { access } from 'fs';
 import { join } from 'path';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
@@ -7,10 +8,16 @@ const pathToWrongFile = join(__dirname, 'files', 'wrongFilename.txt');
 const pathToProperFile = join(__dirname, 'files', 'properFilename.md');
 
 export const rename = async () => {
-  renameFile(pathToWrongFile, pathToProperFile)
-    .catch(() => {
-    throw new Error('FS operation failed')
-  }); 
+  access(pathToProperFile, (err) => {
+    if (err) {
+      renameFile(pathToWrongFile, pathToProperFile)
+        .catch(() => {
+        throw new Error('FS operation failed')
+      });
+    } else {
+      throw new Error('FS operation failed');      
+    }
+  })   
 };
 
 rename();
