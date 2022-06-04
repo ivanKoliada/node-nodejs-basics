91a46405ede2a2ae3ed258d4796737a3775ee59f
justgrace <ivan.koliada69@gmail.com>
2022-06-04 17:50:02 +0300

    feat: create IPC-channel

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -8,8 +8,21 @@ const pathToFile = join(__dirname, 'files', 'script.js');
 const args = process.argv.slice(2);
 
 export const spawnChildProcess = async (args) => {
-  const child = fork(pathToFile, [...args]);
-  child.on('message', ()=> {})
-};
+  const child = fork(pathToFile, [...args], { silent: true }); 
+
+  process.stdin.on('data', data => {
+    console.log(`\x1b[34mSend to child process from master: ${data}\x1b[0m`);
+  });
+    
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
+
+  child.stdout.on('data', data => {
+    console.log(`\x1b[31mReceived from child process: ${data}\x1b[0m`);
+  });
+
+  child.on('exit', _ => console.log('CHILD PROCESS TERMINATED'));
+
+}
 
 spawnChildProcess(args);
